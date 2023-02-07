0440d6f054805f1c713082b66881b614ffb53447
AVIJIT BISWAS <112837308+AVIJIT10-ops@users.noreply.github.com>
2023-02-07 17:37:32 +0600

    assignment -4 complete

--- object.js
+++ object.js
@@ -1,40 +1,66 @@
 
 // Problem-01 Lets play a mind game 
+// please input any positive number and return 
 function mindGame(num) {
+    if (typeof num !== 'number'){
+        return 'Please input a valid number'
+    }
    const  sum = (num * 3 + 10) / 2 - 5;
      return sum;
 }
- const result = mindGame(50)
-console.log(result)
+const k = mindGame(12);
+console.log(k)
+
  
 
+
+
+
 // Problem-2 Finding even or odd 
+// Please input any string and return
 function evenOdd(string) {
+    if (typeof string !== 'string'){
+        return 'Please input a String'
+    }
     if (string.length % 2 === 0) {
         return 'even';
     } else {
         return 'odd';
     }
 }
+const l = evenOdd('ok')
+ console.log(l)
  
- const  x = evenOdd("hello");
- console.log(x)
+
+
+ 
 
 //   Problem-03 is less or gaterthen 
-function  isGSeven(num){
-    if(Math.abs(num-7) < 7){
-        return Math.abs(num-7);
+// please input any number and return
+function  isGSeven(number){
+    if (typeof number !== 'number'){
+        return 'Please input a valid Number'
+    }
+    if(Math.abs(number-7) < 7){
+        return Math.abs(number-7);
     }
     else{
-        return 2* num;
+        return 2* number;
     }
 }
-const s = isGSeven(15);
-console.log(s)
-
-//   Problem-04 Finidng bad Bata
+ const n = isGSeven(15)
+ console.log(n)
  
+ 
+
+
+
+//   Problem-04 Finidng badBata
+//  please  input  any number in array
 function findingBadData(array){
+    if (typeof array !== 'object' ){
+        return 'Please input number in array '
+    }
    let badBataCount = 0;
     for (let i = 0 ; i < array.length; i++){
         if (array [i] < 0){
@@ -43,11 +69,18 @@ function findingBadData(array){
     }
     return badBataCount;
 }
-const d = findingBadData ([2,-5,-7,-13]);
-console.log(d)
+ const w = findingBadData([23,56,87,-90]);
+ console.log(w);
+
+
+
 
 //  Problem-5 Convert your jems into Dimond 
+// please provide  three  valid number 
 function gemsToDiamond(friend1 ,friend2,friend3){
+    if (typeof   friend1  !== 'number'){
+        return 'Please input three valid number'
+    }
     let totalGems = friend1+friend2+friend3;
     let totalDiamonds = totalGems * 2;
     if (totalDiamonds > 1000){
@@ -55,5 +88,10 @@ function gemsToDiamond(friend1 ,friend2,friend3){
     }
     return totalDiamonds;
 }
-const t = gemsToDiamond(21,32,43);
-console.log(t)
+
+const u = gemsToDiamond( 20,200,50);
+console.log(u);
+
+ 
+
+ 
